bac43cb1a3fc6f4cc3b5b9f3542b99c126de91c3
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2021-02-02 23:15:38 +0700

    use routes in server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const routes = require('./router');
+
+routes(app);
+
 app.listen(port, () => {
   console.log(`server running in port ${port}`);
 });
