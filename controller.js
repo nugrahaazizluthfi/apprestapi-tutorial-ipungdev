d4df3a391d6c8f5ffea971077b39acc170b5b4ff
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2021-02-03 10:44:49 +0700

    create show mahasiswa data endpoint

--- controller.js
+++ controller.js
@@ -4,5 +4,12 @@ const response = require('./res');
 const connection = require('./koneksi');
 
 exports.index = function (req, res) {
-  response.ok('aplikasi REST API berjalan dengan baik');
+  response.ok('aplikasi REST API berjalan dengan baik', res);
+};
+
+exports.show = function (req, res) {
+  connection.query('SELECT * FROM mahasiswa', (err, rows, fields) => {
+    if (err) throw err;
+    response.ok(rows, res);
+  });
 };
--- router.js
+++ router.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   let controller = require('./controller');
 
   app.route('/').get(controller.index);
+  app.route('/show-mahasiswa').get(controller.show);
 };
