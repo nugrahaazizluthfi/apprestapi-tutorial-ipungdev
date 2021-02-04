d69b7b3570c80c04d748115578a4ef38b0346d30
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2021-02-04 23:53:24 +0700

    add process login

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,8 @@ const config = require('../config/secret');
 const ip = require('ip');
 
 exports.register = function (req, res) {
-  const { ...post } = req.body;
+  var { ...post } = req.body;
+  post.password = md5(post.password);
 
   var query = `SELECT email FROM ?? WHERE ?? = ?`;
   var table = ['users', 'email', post.email];
@@ -26,6 +27,54 @@ exports.register = function (req, res) {
       });
     } else {
       response.ok('email suda terdaftar!', res);
+    }
+  });
+};
+
+exports.login = function (req, res) {
+  var { ...post } = req.body;
+
+  var query = `SELECT * FROM ?? WHERE ?? = ? AND ?? = ?`;
+  var table = ['users', 'password', md5(post.password), 'email', post.email];
+
+  query = mysql.format(query, table);
+  connection.query(query, function (err, rows) {
+    if (err) throw err;
+
+    if (rows.length == 1) {
+      var token = jwt.sign({ rows }, config.secret, {
+        expiresIn: 1440,
+      });
+
+      user_id = rows[0].id;
+
+      var data = {
+        user_id,
+        access_token: token,
+        ip_address: ip.address(),
+      };
+
+      console.log(data);
+
+      var query = `INSERT INTO ?? SET ?`;
+      var table = ['akses_token'];
+
+      query = mysql.format(query, table);
+
+      connection.query(query, data, function (err, rows) {
+        if (err) throw err;
+        res.json({
+          success: true,
+          message: 'Token JWT tergenerate',
+          token: token,
+          currUser: data.user_id,
+        });
+      });
+    } else {
+      res.json({
+        status: false,
+        message: 'Email atau password salah',
+      });
     }
   });
 };
